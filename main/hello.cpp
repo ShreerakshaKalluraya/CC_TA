0ddef6026b7195e6961bea02093ff7e3cec372ef
ShreerakshaKalluraya <shreerakshabaddadka@gmail.com>
2025-03-13 22:09:16 +0530

    Change

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n  I am Shreeraksha";
-  cout << "Hello, Jenkins\n THis is PES1UG22CS567";
+  cout << "Hello, Jenkins\n i have made this change";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
