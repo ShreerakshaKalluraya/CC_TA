b358785e56455c5e0432a55d4219cfe225568326
ShreerakshaKalluraya <shreerakshabaddadka@gmail.com>
2025-03-13 15:27:22 +0530

    updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, World\n  I am Shreeraksha";
+  cout << "Hello, Jenkins\n THis is PES1UG22CS567";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
